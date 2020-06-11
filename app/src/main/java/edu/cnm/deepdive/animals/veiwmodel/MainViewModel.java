a227b1a5c179f7268fc4af6ec237de49944597f3
Ambar Rodriguez <ambarrodriguez80@yahoo.com>
2020-06-11 17:25:18 -0600

    Toolbar and spinner into land and layout fragments.

--- app/src/main/java/edu/cnm/deepdive/animals/veiwmodel/MainViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/animals/veiwmodel/MainViewModel.java
@@ -1,31 +1,30 @@
 package edu.cnm.deepdive.animals.veiwmodel;
 
 import android.app.Application;
-import android.os.AsyncTask;
-import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.animals.BuildConfig;
 import edu.cnm.deepdive.animals.model.Animal;
 import edu.cnm.deepdive.animals.service.AnimalService;
-import java.io.IOException;
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
 import java.util.List;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.Retrofit.Builder;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainViewModel extends AndroidViewModel {
 
   private MutableLiveData<List<Animal>> animals;
+  private MutableLiveData<Throwable> throwable;
+  private MutableLiveData<Integer> selectedItem;
+  private AnimalService animalService;
 
   public MainViewModel(@NonNull Application application) {
     super(application);
+    animalService = AnimalService.getInstance();
     animals = new MutableLiveData<>();
+    throwable = new MutableLiveData<>();
+    selectedItem = new MutableLiveData<>();
     loadAnimals();
   }
 
@@ -33,50 +32,30 @@ public class MainViewModel extends AndroidViewModel {
     return animals;
   }
 
+  public LiveData<Throwable> getThrowable() {
+    return throwable;
+  }
+
+  public LiveData<Integer> getSelectedItem() {
+    return selectedItem;
+  }
+
+  public void select(int index) {
+    selectedItem.setValue(index);
+  }
+
   private void loadAnimals() {
 
-    new AsyncTask<Void, Void, List<Animal>>() {
+    animalService.getAnimals(BuildConfig.CLIENT_KEY)
+        .subscribeOn(Schedulers.io())
+        .subscribe(
+            (animals) -> {
+              this.animals.postValue(animals);
+              selectedItem.postValue(0);
+            },
+            (throwable) -> this.throwable.postValue(throwable));
 
-      AnimalService animalService;
-
-      @Override
-      protected void onPreExecute() {
-        super.onPreExecute();
-
-        Gson gson = new GsonBuilder()
-            .excludeFieldsWithoutExposeAnnotation()
-            .create();
-        Retrofit retrofit = new Builder()
-            .baseUrl(BuildConfig.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build();
-
-        animalService = retrofit.create(AnimalService.class);
-      }
-      @Override
-      protected List<Animal> doInBackground(Void... voids) {
-
-        try {
-          Response<List<Animal>> response = animalService.getAnimals(BuildConfig.CLIENT_KEY)
-              .execute();
-          if (response.isSuccessful()) {
-            List<Animal> animals = response.body();
-            assert animals != null;
-            MainViewModel.this.animals.postValue(animals);
-            return animals;
-
-          } else {
-            Log.e("AnimalService", response.message());
-            cancel(true);
-          }
-
-        } catch (IOException e) {
-          Log.e("AnimalService", e.getMessage(), e);
-          cancel(true);
-        }
-        return null;
-      }
-
-    }.execute();
   }
+
 }
+
